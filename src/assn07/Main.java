9a459a338db1d4c35c06d40b7edbe8476ef3b43f
William Millen <157399497+wvmillen@users.noreply.github.com>
2024-04-27 15:11:59 -0400

    New projects

---
+++ src/assn07/Main.java
@@ -0,0 +1,23 @@
+package assn07;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Map<String,String> passwordManager = new PasswordManager<>();
+
+        // your code below
+
+        // infite loop to go back to "Enter master password"
+
+
+
+        // loop to read and execute commands until "Exit" is entered
+
+
+
+
+    }
+}
